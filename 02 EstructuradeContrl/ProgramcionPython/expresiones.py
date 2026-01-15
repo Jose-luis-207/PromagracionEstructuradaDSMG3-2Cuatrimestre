b3685105a4cb3501a17f1718d0706e543c456d1a
LuisGH <luis.gar.hernandez2007@gmail.com>
2026-01-15 12:56:01 -0600

    Primer practica de python

---
+++ 02 EstructuradeContrl/ProgramcionPython/expresiones.py
@@ -0,0 +1,33 @@
+"""En esta parte de codigo se va a declarar varios tipos de variables """
+# Expresiones Aritmeticas
+suma = 3+ 5
+resta = 10 - 12
+multi = 34 - 23
+div = 10 / 2
+divEntera = 12 // 5 
+mod = 10 % 2
+potencia = Math.pow(4, 2)
+"""
+salida = f"Hola soy la suma: (suma)"
+print(salida)   
+salida = 'Hola soy la suma' + suma
+print(salida) 
+salida = 'Hola soy la "suma" ' + suma
+print(salida) 
+salida = "Hola soy la 'suma' :" + suma  
+print(salida) 
+"""
+# Intercomentarios de string 
+salida = f"""
+         Expresiones Aritmeticas =====
+          La suma es : {suma}
+          La resta es: {resta}
+          La multiplicacion es : {multi}
+          La division es : {div}
+          La division Entera es : {div} 
+          La potencia es : {potencia}
+          El modulo es : {mod}
+          """
+print(salida)
+
+          
